3744f3f05346ca8d3832e0f393ed85cf294e585a
Chris Freeman <cfreeman.code@gmail.com>
2016-09-08 23:45:38 -0500

    automatically transtion to learning route when entering application

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.$.getJSON('resources');
+  },
+
+  afterModel() {
+    this.transitionTo('learning');
   }
 });
