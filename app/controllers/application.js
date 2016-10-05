80a3cb05571b5c6adc70ed6f91ca6106cece5e53
Stephanie Riera <stephanie@frontside.io>
2016-10-05 11:43:57 -0500

    Remove console logs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,26 +8,12 @@ export default Ember.Controller.extend({
   filteredMedia: Ember.computed('type', 'model', function() {
     var chosenType = this.get('type');
     var mediaList = this.get('model');
-    console.log('filteredmediahere');
 
     if (chosenType == null) {
       return mediaList;
     } return mediaList.filter( item => {
       return item.type === chosenType;
     });
-  }),
-
-  logType: Ember.observer('type', function() {
-    console.log('type changed', this.get('type'));
-
-  }),
-
-  logModel: Ember.observer('model', function() {
-    console.log('model changed', this.get('model'));
-  }),
-
-  logFilteredMedia: Ember.observer('filteredMedia', function() {
-    console.log('FM changed', this.get('filteredMedia'));
   }),
 
   actions: {
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log(params);
     return Ember.$.getJSON('resources');
   }
 });
